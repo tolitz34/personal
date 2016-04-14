e9e570f3cc64265284548d7bb0b0a59b7ec2ffaf
Joselito Pasculado <tolitz34@gmail.com>
2016-04-14 15:40:04 +0800

    change the recent works

---
+++ resources/views/project1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta http-equiv="CONTENT-TYPE" content="text/html; charset=utf-8" />
+    <title>Freelance</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    {!! Html::style('css/style.css') !!}
+    {!! Html::style('css/bootstrap.min.css') !!}
+    {!! Html::style('fonts/font-awesome/css/font-awesome.css') !!}
+    <!--    <link rel="stylesheet" href="css/font-awesome.min.css">-->
+
+</head>
+
+<body>
+
+
+</body>
+</html>
