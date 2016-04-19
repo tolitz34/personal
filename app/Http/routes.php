753dfcc18bdabd1762f0c65ddf91b5757fbe2111
Joselito Pasculado <tolitz34@gmail.com>
2016-04-19 10:20:51 +0800

    add blade for chart

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,10 +10,10 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+//
+//Route::>>get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('/', 'GuestController@index');
 Route::get('blog', 'GuestController@blog');
