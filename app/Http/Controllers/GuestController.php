fc1b39d4e6f9fd60e93ab2bd51a001d8212d956b
Joselito Pasculado <tolitz34@gmail.com>
2016-04-19 10:20:34 +0800

    add blade for chart

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Khill\Lavacharts\Lavacharts;
 
 class GuestController extends Controller
 {
@@ -18,5 +19,34 @@ class GuestController extends Controller
     public function blog()
     {
         return view('blog');
+    }
+
+    public function generatecharts()
+    {
+        $lava = new Lavacharts;
+        $stocksTable = $lava->DataTable();
+
+        $stocksTable->addDateColumn('Day of Month')
+                    ->addNumberColumn('Project')
+                    ->addNumberColumn('Official');
+
+        // Random Data For Example
+
+        for ($a = 1; $a < 30; $a++)
+        {
+            $rowData = array(
+                "2014-8-$a", rand(800, 1000), rand(800, 1000)
+            );
+
+            $stocksTable->addRow($rowData);
+        }
+
+        $chart = $lava->LineChart('Stocks')
+                      ->setOptions(array(
+                         'datatable' => $stocksTable,
+                         'title' => 'Stock Market Trends'
+            ));
+
+        return view('widget');
     }
 }
