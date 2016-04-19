99c3fec5c438d59ffcb21ab188afbe393bed9ed7
Joselito Pasculado <tolitz34@gmail.com>
2016-04-19 10:21:30 +0800

    add blade for chart

--- resources/views/widget.blade.php
+++ resources/views/widget.blade.php
@@ -9,72 +9,17 @@
 
 @endsection
 
-@section('css')
-   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+@section ('content')
+
+<div class="row">
+    <div class="page-header">
+        <h2>Welcome to analytics</h2>
+    </div>
+
+</div>
+<div id="stock-div"></div>
+echo $lava->render('LineChart', 'Stocks', 'stock-div');
+@linechart('Stocks', 'stocks-div');
+
 
 @endsection
-
-
-@section('widget.noresults')
-
-   <br>
-
-@include('widget.searchform')
-
-    @if(isset($results))
-
-
-   @include('widget.searchresults')
-
-   @else
-
-   <h1>Widget Stats</h1>
-
-     // insert a div id with a value of chart
-
-
-     <hr>
-
-
-   @include('widget.allrecords')
-
-   @endif
-
-@endsection
-
-
-@section('scripts')
-
-            //cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js
-            //cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js
-
-
-      function confirmDelete()
-   {
-
-       var x = confirm("Are you sure you want to delete?");
-
-      if (x)
-          return true;
-
-      else
-
-         return false;
-
-   }
-
-
-    var data = ;
-
-     Morris.Line({
-
-         element: 'chart',
-         data: data,
-         xkey: 'year',
-         labels: ['widgets created']
-
-     });
-
-
-@endsectionn
-
